58b3f203f3880b430e911e99407911b43f72b9e4
sergey.makhno <11550@mail.ru>
2025-12-16 15:23:12 +0300

    [easy] LongestCommonPrefix

---
+++ src/main/java/easy/LongestCommonPrefix.java
@@ -0,0 +1,46 @@
+package easy;
+
+import java.util.Arrays;
+
+/**
+ * Write a function to find the longest common prefix string amongst an array of strings.
+ *
+ * If there is no common prefix, return an empty string "".
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: strs = ["flower","flow","flight"]
+ * Output: "fl"
+ * Example 2:
+ *
+ * Input: strs = ["dog","racecar","car"]
+ * Output: ""
+ * Explanation: There is no common prefix among the input strings.
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= strs.length <= 200
+ * 0 <= strs[i].length <= 200
+ * strs[i] consists of only lowercase English letters if it is non-empty.
+ */
+class LongestCommonPrefix {
+
+    public String longestCommonPrefix(String[] strs) {
+        Arrays.sort(strs);
+        String first = strs[0];
+        String last = strs[strs.length - 1];
+        int idx = 0;
+        while (idx < first.length() && idx < last.length()) {
+            if (first.charAt(idx) == last.charAt(idx)) {
+                idx++;
+            } else {
+                break;
+            }
+        }
+        return first.substring(0, idx);
+    }
+
+}
---
+++ src/test/java/easy/LongestCommonPrefixTest.java
@@ -0,0 +1,28 @@
+package easy;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class LongestCommonPrefixTest {
+
+    private final LongestCommonPrefix longestCommonPrefix = new LongestCommonPrefix();
+
+    private static Stream<Arguments> longestCommonPrefix_shouldBeEquals() {
+        return Stream.of(
+                Arguments.of(new String[]{"flower","flow","flight"}, "fl"),
+                Arguments.of(new String[]{"flower","f","flight"}, "f"),
+                Arguments.of(new String[]{"dog","racecar","car"}, "")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void longestCommonPrefix_shouldBeEquals(String[] input, String expected) {
+        assertEquals(expected, longestCommonPrefix.longestCommonPrefix(input));
+    }
+
+}
