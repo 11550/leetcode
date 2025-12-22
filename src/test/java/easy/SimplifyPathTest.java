958d348d093fb270be46d9f6d00c37b4cf9ce841
sergey.makhno <11550@mail.ru>
2025-12-23 01:30:21 +0300

    [easy] SimplifyPath

---
+++ src/test/java/easy/SimplifyPathTest.java
@@ -0,0 +1,32 @@
+package easy;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class SimplifyPathTest {
+
+    private static Stream<Arguments> simplifyPath_shouldBeEquals() {
+        return Stream.of(
+//                Arguments.of("/home/", "/home"),
+//                Arguments.of("/home//foo/", "/home/foo"),
+//                Arguments.of("/home/user/Documents/../Pictures", "/home/user/Pictures"),
+//                Arguments.of("/../", "/"),
+//                Arguments.of("/a/./b/../../c/", "/c"),
+                Arguments.of("/a//b////c/d//././/..", "/a/b/c"),
+                Arguments.of("/.../a/../b/c/../d/./", "/.../b/d")
+        );
+    }
+
+    private final SimplifyPath simplifyPath = new SimplifyPath();
+
+    @ParameterizedTest
+    @MethodSource
+    void simplifyPath_shouldBeEquals(String input, String expected) {
+        assertEquals(expected, simplifyPath.simplifyPath(input));
+    }
+
+}
