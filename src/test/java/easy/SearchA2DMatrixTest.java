5363823506f340649720da77f560aee719c05677
sergey.makhno <11550@mail.ru>
2026-01-06 22:55:31 +0300

    [easy] SearchA2DMatrixTest

--- src/test/java/easy/SearchA2DMatrixTest.java
+++ src/test/java/easy/SearchA2DMatrixTest.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 class SearchA2DMatrixTest {
     
-    private static Stream<Arguments> setZeroes_shouldBeEquals() {
+    private static Stream<Arguments> searchMatrix_shouldBeEquals() {
         return Stream.of(
                 Arguments.of(new int[][]{{1,3,5,7},{10,11,16,20},{23,30,34,60}}, 3, true),
                 Arguments.of(new int[][]{{1,3,5,7},{10,11,16,20},{23,30,34,60}}, 13, false)
@@ -20,7 +20,7 @@ class SearchA2DMatrixTest {
 
     @ParameterizedTest
     @MethodSource
-    void setZeroes_shouldBeEquals(int[][] input, int target, boolean expected) {
+    void searchMatrix_shouldBeEquals(int[][] input, int target, boolean expected) {
         assertEquals(expected, searchA2DMatrix.searchMatrix(input, target));
     }
 
